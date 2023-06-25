a2345214ef226623b7818c6b7f30561ee35299f9
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-06-24 22:40:16 -0300

    Primeiro commit. Criar funcao main e variaveis

---
+++ monitorador-de-sites.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nome string = "Márcio"
+	var versao float32 = 1.1
+	fmt.Println("olá, Sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+	
+}
