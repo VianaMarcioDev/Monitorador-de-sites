0365cee3a5f8893ce5d18b3678871a1c0ce5ddbe
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-06-25 14:56:14 -0300

    mudar para operador curto de declaracao

--- monitorador-de-sites.go
+++ monitorador-de-sites.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main(){
-	var nome string = "Márcio"
-	var versao float32 = 1.1
+	nome := "Márcio"
+	versao := 1.1
 	fmt.Println("olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 	
