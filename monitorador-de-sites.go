3f418f0f79421db5fb99d303dd6504aa29b9bca6
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-06-26 19:46:21 -0300

    Implementar monitoramento com pacote net/http

--- monitorador-de-sites.go
+++ monitorador-de-sites.go
@@ -2,26 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	boasVindas()
-	menu()
-
-	var opcaoEscolhida int
-	/*
-		fmt.Scanf("%d", &opcaoEscolhida) Scanf precisa determinar o tipo
-	*/
-	fmt.Scan(&opcaoEscolhida)
+	menu()	
+	opcaoEscolhida := leComando()
 
 	switch opcaoEscolhida {
 	case 0:
 		fmt.Println("Saindo do programa.")
 		os.Exit(0)
 	case 1:
-		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo logs...")
 	default:
@@ -42,4 +38,20 @@ func menu() {
 	fmt.Println("1 - inciar Monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do Programa")
+}
+func leComando() int{
+	var opcaoEscolhida int 
+	fmt.Scan(&opcaoEscolhida)
+	fmt.Println("O comando escolhido foi", opcaoEscolhida)
+}
+
+func iniciarMonitoramento(){
+	fmt.Println("Monitorando...")
+	site := "http://www.alura.com.br/"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200{
+		fmt.Println("Site", site,"foi carregado com sucesso!")
+	}else{
+		fmt.Println("Site com problemas! Staus do problema", resp.StatusCode)
+	}
 }
