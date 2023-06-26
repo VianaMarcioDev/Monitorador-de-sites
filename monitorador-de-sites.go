558709f414eaa3575e6fc0bfde7040c6f3bf7cce
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-06-26 19:00:47 -0300

    Organizar codigo em funcoes

--- monitorador-de-sites.go
+++ monitorador-de-sites.go
@@ -1,29 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	nome := "Márcio"
-	versao := 1.1
-	fmt.Println("olá, Sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
+func main() {
 
-	fmt.Println("1 - inciar Monitoramento")
-	fmt.Println("2 - Exibir logs")
-	fmt.Println("0 - Sair do Programa")
+	boasVindas()
+	menu()
 
 	var opcaoEscolhida int
 	/*
-	fmt.Scanf("%d", &opcaoEscolhida) Scanf precisa determinar o tipo
+		fmt.Scanf("%d", &opcaoEscolhida) Scanf precisa determinar o tipo
 	*/
 	fmt.Scan(&opcaoEscolhida)
 
-	switch opcaoEscolhida{
-		case 0: fmt.Println("Saindo do programa.")		
-		case 1: fmt.Println("Monitorando...")
-		case 2: fmt.Println("Exibindo logs...")
-		default: fmt.Println("Opção inválida!")
+	switch opcaoEscolhida {
+	case 0:
+		fmt.Println("Saindo do programa.")
+		os.Exit(0)
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	default:
+		fmt.Println("Opção inválida!")
+		os.Exit(-1)
 	}
 
-	fmt.Println("O comando escolhido foi ", opcaoEscolhida)
+}
+
+func boasVindas() {
+	nome := "Márcio"
+	versao := 1.1
+	fmt.Println("Olá, Sr./Sra.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func menu() {
+	fmt.Println("1 - inciar Monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do Programa")
 }
