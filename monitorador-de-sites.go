440e9a79097608f50ef0c7b241068c688b664f2e
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-06-26 18:27:33 -0300

    Usar a funcao Scan

--- monitorador-de-sites.go
+++ monitorador-de-sites.go
@@ -7,5 +7,16 @@ func main(){
 	versao := 1.1
 	fmt.Println("olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
-	
+
+	fmt.Println("1 - inciar Monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var opcaoEscolhida int
+	/*
+	fmt.Scanf("%d", &opcaoEscolhida) Scanf precisa determinar o tipo
+	*/
+	fmt.Scan(&opcaoEscolhida)
+
+	fmt.Println("O comando escolhido foi ", opcaoEscolhida)
 }
